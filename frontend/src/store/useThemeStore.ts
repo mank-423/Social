a02129377e32d03dc90076a36eb1bf0fbbcb156c
Mayank Kumar <mk4664348@gmail.com>
2025-08-08 00:21:35 +0530

    favicon, setting theme bug fix

--- frontend/src/store/useThemeStore.ts
+++ frontend/src/store/useThemeStore.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import type { ThemeStore } from "../types/store";
 
 export const useThemeStore = create<ThemeStore>((set) => ({
-    theme: localStorage.getItem('char-theme') || 'light',
+    theme: localStorage.getItem('chat-theme') || 'forest',
 
     setTheme: (theme: string) => {
         localStorage.setItem('chat-theme', theme)
